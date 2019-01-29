40fa4ea742270967b1c8c72906135481eb688c1a
Matt <bpmjg3@gmail.com>
2019-01-29 15:30:45 -0800

    redux-state

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import firebase from '../../firebase';
+import md5 from 'md5';
 import { Grid, Form, Segment, Button, Header, Message, Icon } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
@@ -10,10 +11,49 @@ class Register extends React.Component {
             username: '',
             email: '',
             password: '',
-            passwordConfirmation: ''
+            passwordConfirmation: '',
+            errors: [],
+            loading: false,
+            usersRef: firebase.database().ref('users')
         }
         this.handleChange = this.handleChange.bind(this);
     }
+
+    isFormValid = () => {
+        let errors = [];
+        let error;
+
+        if (this.isFormEmpty(this.state)) {
+            // throw error
+            error = { message: 'Fill in all fields' };
+            this.setState({ errors: errors.concat(error) });
+            return false;
+        } else if (!this.isPasswordValid(this.state)) {
+            // throw error
+            error = { message: 'Password is invalid'};
+            this.setState({ errors: errors.concat(error) });
+            return false;
+        } else {
+            // form valid
+            return true;
+        }
+    }
+
+    isFormEmpty = ({ username, email, password, passwordConfirmation }) => {
+        return !username.length || !email.length || !password.length || !passwordConfirmation.length;
+    }
+
+    isPasswordValid = ({ password, passwordConfirmation }) => {
+        if (password.length < 6 || passwordConfirmation < 6) {
+            return false;
+        } else if (password !== passwordConfirmation) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    displayErrors = errors => errors.map((error, i) => <p key={i}>{error.message}</p>);
 
     handleChange = (event) => {
         this.setState({ [event.target.name]: event.target.value })
@@ -21,27 +61,61 @@ class Register extends React.Component {
 
     handleSubmit = (event) => {
         event.preventDefault();
-        firebase
-            .auth()
-            .createUserWithEmailAndPassword(this.state.email, this.state.password)
-            .then(createdUser => {
-                console.log(createdUser);
-            })
-            .catch(err => {
-                console.log(err);
-            })
+        if (this.isFormValid()) {
+            this.setState({ errors: [], loading: true})
+            firebase
+                .auth()
+                .createUserWithEmailAndPassword(this.state.email, this.state.password)
+                .then(createdUser => {
+                    console.log(createdUser);
+                    createdUser.user.updateProfile({
+                        displayName: this.state.username,
+                        photoURL: `http://gravatar.com/avatar/${md5(createdUser.user.email)}?d=identicon`
+                    })
+                    .then(() => {
+                        this.saveUser(createdUser).then(() => {
+                            console.log('user saved');
+                        })
+                    })
+                    .catch((err) => {
+                        console.error(err);
+                        this.setState({ errors: this.state.errors.concat(err), loading: false });
+                    })
+                    // this.setState({ loading: false });
+                })
+                .catch(err => {
+                    console.error(err);
+                    this.setState({ errors: this.state.errors.concat(err), loading: false })
+                })
+        }
+    }
+
+    saveUser = createdUser => {
+        return this.state.usersRef.child(createdUser.user.uid).set({
+            name: createdUser.user.displayName,
+            avatar: createdUser.user.photoURL
+        });
+    }
+
+    handleInputError = (errors, inputName) => {
+        return errors.some(error => 
+            error.message.toLowerCase().includes(inputName)
+        ) ? 
+        'error' 
+        : 
+        ''
     }
 
     render() {
 
-        const { username, email, password, passwordConfirmation } = this.state;
+        const { username, email, password, passwordConfirmation, errors, loading } = this.state;
 
         return (
             <Grid textAlign="center" verticalAlign="middle" className="app">
                 <Grid.Column style={{ maxWidth: 450 }}>
-                    <Header as="h2" icon color="green" textAlign="center">
-                        <Icon name="leaf" color="green" />
-                        Register for TeamChat
+                    <Header as="h1" icon color="violet" textAlign="center">
+                        <Icon name="leaf" color="violet" />
+                        Register
                     </Header>
                     <Form size="large" onSubmit={this.handleSubmit}>
                         <Segment stacked>
@@ -49,17 +123,26 @@ class Register extends React.Component {
                             placeholder="Username" onChange={this.handleChange} value={username} type="text" />
                             
                             <Form.Input fluid name="email" icon="mail" iconpositon="left"
-                            placeholder="Email Address" onChange={this.handleChange} value={email} type="email" />
+                            placeholder="Email Address" onChange={this.handleChange} value={email} type="email" 
+                            className={this.handleInputError(errors, 'email')} />
 
                             <Form.Input fluid name="password" icon="lock" iconpositon="left"
-                            placeholder="Password" onChange={this.handleChange} value={password} type="password" />
+                            placeholder="Password" onChange={this.handleChange} value={password} type="password" 
+                            className={this.handleInputError(errors, 'password')} />
 
                             <Form.Input fluid name="passwordConfirmation" icon="repeat" iconpositon="left"
-                            placeholder="Password Confirmation" onChange={this.handleChange} value={passwordConfirmation} type="password" />
+                            placeholder="Password Confirmation" onChange={this.handleChange} value={passwordConfirmation} type="password" 
+                            className={this.handleInputError(errors, 'password')} />
 
-                            <Button color="green" fluid size="large">Submit</Button>
+                            <Button disabled={loading} className={loading ? 'loading' : ''} color="violet" fluid size="large">Submit</Button>
                         </Segment>
                     </Form>
+                    {errors.length > 0 && (
+                        <Message error>
+                            <h3>Error</h3>
+                            {this.displayErrors(this.state.errors)}
+                        </Message>
+                    )}
                     <Message>Already a user? <Link to="/login">Login</Link></Message>
                 </Grid.Column>
             </Grid>
--- src/index.js
+++ src/index.js
@@ -4,20 +4,64 @@ import App from './components/App';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import registerServiceWorker from './registerServiceWorker';
+import firebase from './firebase';
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers';
+import { setUser, clearUser } from './actions';
 
-import 'semantic-ui-css/semantic.min.css'
+import Spinner from './Spinner';
+import 'semantic-ui-css/semantic.min.css';
 
-const Root = () => (
-    <Router>
-        <Switch>
-            <Route exact path="/" component={App}/>
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/register" component={Register} />
-        </Switch>
-    </Router>
+const store = createStore(rootReducer, composeWithDevTools());
+class Root extends React.Component {
+
+    componentDidMount() {
+        // console.log(this.props.isLoading);
+        firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                console.log(user);  // all info for currently signed in user
+                this.props.setUser(user);
+                this.props.history.push('/');
+            } else {
+                this.props.history.push('/login');
+                this.props.clearUser();
+            }
+        })
+    }
+
+    render () {
+        return this.props.isLoading ? 
+        <Spinner /> 
+        : (
+            <Switch>
+                <Route exact path="/" component={App}/>
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/register" component={Register} />
+            </Switch>
+        );
+    }
+}
+
+const mapStateToProps = state => ({
+    isLoading: state.user.isLoading
+})
+
+const RootWithAuth = withRouter(
+    connect(
+        mapStateToProps, 
+        { setUser, clearUser }
+    )(Root)
 );
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+ReactDOM.render(
+<Provider store={store}>
+    <Router>
+        <RootWithAuth />
+    </Router>
+</Provider>
+, document.getElementById('root'));
 registerServiceWorker();
