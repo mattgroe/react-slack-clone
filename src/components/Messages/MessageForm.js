67864576b51ba9627ead58e95a8bf4e84e8095a4
Matt <bpmjg3@gmail.com>
2019-01-31 15:24:12 -0800

    remove yarn lock

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import firebase from '../../firebase';
 import { Segment, Button, Input } from 'semantic-ui-react';
+import FileModal from './FileModal';
 
 class MessageForm extends React.Component {
     state = {
@@ -8,8 +9,12 @@ class MessageForm extends React.Component {
         channel: this.props.currentChannel,
         user: this.props.currentUser,
         loading: false,
-        errors: []
+        errors: [],
+        modal: false
     }
+
+    openModal = () => this.setState({ modal: true });
+    closeModal = () => this.setState({ modal: false });
 
     handleChange = event => {
         this.setState({ [event.target.name]: event.target.value })
@@ -51,7 +56,7 @@ class MessageForm extends React.Component {
     }
 
     render () {
-        const { errors, message, loading } = this.state;
+        const { errors, message, loading, modal } = this.state;
 
         return (
             <Segment className="message__form">
@@ -82,8 +87,13 @@ class MessageForm extends React.Component {
                     <Button 
                         color="teal"
                         content='Upload Media'
+                        onClick={this.openModal}
                         labelPosition='right'
                         icon='cloud upload'
+                    />
+                    <FileModal 
+                        modal={modal}
+                        closeModal={this.closeModal}
                     />
                 </Button.Group>
             </Segment>
