f64081bf30a15b9fa60f0dd63575e5395821dcdf
Matt <bpmjg3@gmail.com>
2019-01-31 02:35:31 -0800

    adding messages to db

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -2,20 +2,53 @@ import React from 'react';
 import { Segment, Comment } from 'semantic-ui-react';
 import MessagesHeader from './MessagesHeader';
 import MessageForm from './MessageForm';
+import firebase from '../../firebase';
 
 class Messages extends React.Component {
+    state = {
+        messagesRef: firebase.database().ref('messages'),
+        channel: this.props.currentChannel,
+        user: this.props.currentUser
+    }
+
+    componentDidMount() {
+        const { channel, user } = this.state;
+
+        if (channel && user) {
+            this.addListeners(channel.id);
+        }
+    }
+
+    addListeners = channelId => {
+        this.addMessageListener(channelId);
+    }
+
+    addMessageListener = channelId => {
+        let loadedMessages = [];
+        this.state.messagesRef.child(channelId).on('child_added', snap => {
+            loadedMessages.push(snap.val());
+            console.log(loadedMessages);
+        })
+    }
+
     render() {
+        const { messagesRef, channel, user } = this.state;
+
         return (
             <React.Fragment>
                 <MessagesHeader />
 
                 <Segment>
                     <Comment.Group className="messages">
-                        
+
                     </Comment.Group>
                 </Segment>
 
-                <MessageForm />
+                <MessageForm 
+                    messagesRef={messagesRef}
+                    currentChannel={channel}
+                    currentUser={user}
+                />
             </React.Fragment>
         )
     }
